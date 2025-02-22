8b220d2a9ef153556dced6ef8ca9af98669a3168
d768092 <d768092@gmail.com>
2025-02-22 15:50:54 +0800

    Implement verifier except aggregate_comm

--- plonkish_backend/src/backend/plookup/verifier.rs
+++ plonkish_backend/src/backend/plookup/verifier.rs
@@ -1,38 +1,37 @@
-use std::ops::Div;
+use halo2_curves::ff::WithSmallOrderMulGroup;
 
 use crate::{
-    pcs::PolynomialCommitmentScheme,
+    pcs::{PolynomialCommitmentScheme,Evaluation},
     poly::univariate::UnivariatePolynomial,
     util::{arithmetic::PrimeField, transcript::TranscriptRead},
-    halo2_curves::ff::WithSmallOrderMulGroup,
     Error,
 };
 
 use super::{
     PlookupVerifierParam,
-    util::{aggregate_field, aggregate_poly},
+    util::{aggregate_field, aggregate_comm},
 };
 
-pub(super) fn prove<
+pub(super) fn verify<
     F: PrimeField + WithSmallOrderMulGroup<3>, 
-    Pcs: PolynomialCommitmentScheme<F, Polynomial = UnivariatePolynomial<F>>
+    Pcs: PolynomialCommitmentScheme<F, Polynomial = UnivariatePolynomial<F>>,
 >(
     vp: PlookupVerifierParam<F, Pcs>,
     transcript: &mut impl TranscriptRead<Pcs::CommitmentChunk, F>,
-) -> Result<bool, Error> {
-    let f_poly_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
-    let h1_poly_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
-    let h2_poly_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
+) -> Result<(), Error> {
+    let f_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
+    let h1_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
+    let h2_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
     let mut challenges: Vec<F> = Vec::with_capacity(2);
     challenges.extend(transcript.squeeze_challenges(2));
     let beta = &challenges[0];
     let gamma = &challenges[1];
-    let z_poly_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
+    let z_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
     let delta = &transcript.squeeze_challenge();
-    let q_poly_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
+    let q_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
     let zeta = &transcript.squeeze_challenge();
     // [f_eval, h1_eval, h2_eval, z_eval]
-    let evals = transcript.read_field_elements(4)?;
+    let mut evals = transcript.read_field_elements(4)?;
     // [h1_g_eval, h2_g_eval, z_g_eval]
     let g_evals = transcript.read_field_elements(3)?;
     let q_eval = compute_quotient_polynomial_eval(
@@ -40,10 +39,22 @@ pub(super) fn prove<
         &evals[0], &evals[1], &evals[2], &evals[3],
         &g_evals[0], &g_evals[1], &g_evals[2]
     );
+    evals.push(q_eval);
     let eps = &transcript.squeeze_challenge();
-    let agg_witness_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
-    let agg_g_witness_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
-    Ok(true)
+    let agg_comm = aggregate_comm::<F, Pcs>(eps, vec![&f_comm, &h1_comm, &h2_comm, &z_comm, &q_comm]);
+    let agg_g_comm = aggregate_comm::<F, Pcs>(eps, vec![&h1_comm, &h2_comm, &z_comm]);
+    let agg_eval = aggregate_field(eps, evals.iter().collect());
+    let agg_g_eval = aggregate_field(eps, g_evals.iter().collect());
+    // don't need to read these commitment because they're read in Pcs::verify()
+    // let agg_witness_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
+    // let agg_g_witness_comm = Pcs::read_commitment(&vp.pcs, transcript)?;
+    // Pcs::verify(&vp.pcs, &agg_comm, zeta, &agg_eval, transcript)?;
+    // Pcs::verify(&vp.pcs, &agg_g_comm, &(vp.g*zeta), &agg_g_eval, transcript)?;
+    let agg_comms = [&agg_comm, &agg_g_comm];
+    let agg_points = [*zeta, vp.g*zeta];
+    let agg_evals = [Evaluation::new(0, 0, agg_eval), Evaluation::new(0, 0, agg_g_eval)];
+    Pcs::batch_verify(&vp.pcs, agg_comms, &agg_points, &agg_evals, transcript)?;
+    Ok(())
 }
 
 fn compute_quotient_polynomial_eval<F: PrimeField+WithSmallOrderMulGroup<3>>(
@@ -75,7 +86,18 @@ fn compute_quotient_polynomial_eval<F: PrimeField+WithSmallOrderMulGroup<3>>(
     };
     let ln_eval = ln_poly.evaluate(zeta);
     let a = l0_eval * (*z_eval - F::ONE);
-    let b = F::ZERO; // TODO
+    let b = {
+        let t_poly = UnivariatePolynomial::lagrange(t.clone()).ifft();
+        let t_eval = t_poly.evaluate(zeta);
+        let t_g_eval = t_poly.evaluate(&(*g*zeta));
+        let front = *zeta - F::invert(g).unwrap();
+        let beta_plus_1 = F::ONE + beta;
+        let lhs = *z_eval * beta_plus_1 * (*f_eval + *gamma) *
+            (t_eval + t_g_eval * beta + beta_plus_1 * gamma);
+        let rhs = *z_g_eval * (*h1_eval + *h1_g_eval * beta + beta_plus_1 * gamma) *
+            (*h2_eval + *h2_g_eval * beta + beta_plus_1 * gamma);
+        front * (lhs - rhs)
+    };
     let c = ln_eval * (*h1_eval - h2_g_eval);
     let d = ln_eval * (*z_eval - F::ONE);
     let vanish = { // x^n - 1
